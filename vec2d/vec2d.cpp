e82b2c07f31217de14ad07d2894c931b354870ea
Nick Reed <donovanreed418@gmail.com>
2019-06-22 21:18:32 -0400

    vector system is now fully functional

--- vec2d/vec2d.cpp
+++ vec2d/vec2d.cpp
@@ -1,26 +1,105 @@
 #include "vec2d.h"
+#include <iostream>
+#include <cmath>
 
 
-  //these operators let us return values with array notation
-  double &vec2d::operator [](int i) {
-    //return a reference
-    switch(i) {
-      case 0:
-        return x;
-        break;
-      case 1:
-        return y;
-        break;
-    }
+//these operators let us return values with array notation
+double &vec2d::operator [](int i) {
+  //return a reference
+  switch(i) {
+    case 0:
+      return x;
+      break;
+    case 1:
+      return y;
+      break;
   }
-  double vec2d::operator [](int i) const {
-    //return a const val
-    switch(i) {
-      case 0:
-        return x;
-        break;
-      case 1:
-        return y;
-        break;
-    }
+}
+double vec2d::operator [](int i) const {
+  //return a const val
+  switch(i) {
+    case 0:
+      return x;
+      break;
+    case 1:
+      return y;
+      break;
   }
+}
+
+//-----------------------------------------------------------------------------
+//scalar const operations
+vec2d vec2d::operator+(double scalar_val) const {
+  return vec2d(x + scalar_val, y + scalar_val);
+}
+
+vec2d vec2d::operator-(double scalar_val) const {
+  return vec2d(x - scalar_val, y - scalar_val);
+}
+
+vec2d vec2d::operator*(double scalar_val) const {
+  return vec2d(x * scalar_val, y * scalar_val);
+}
+
+vec2d vec2d::operator/(double scalar_val) const {
+  if(scalar_val == 0) { throw "can't divide vector by 0!\n"; }
+  return vec2d(x / scalar_val, y / scalar_val);
+}
+
+vec2d vec2d::operator-() const {
+  return vec2d(x * -1, y * -1);
+}
+
+
+//-----------------------------------------------------------------------------
+//vector const operations
+
+vec2d vec2d::operator+(const vec2d &v) const {
+  return vec2d(x + v[0], y + v[1]);
+}
+
+//-----------------------------------------------------------------------------
+//related to vector properties
+
+double vec2d::magnitude() const {
+  return sqrt((x * x) + (y * y));
+}
+
+double vec2d::magnitudeSquared() const {
+  return ((x * x) + (y * y));
+}
+
+vec2d vec2d::normalize() const {
+  //NOTE: will not normalize beyond .001, returns this
+  double m = this->magnitude();
+  if(m < .001 && m > -.001) {
+    return *this;
+  }
+  return vec2d(x / m, y / m);
+}
+
+double vec2d::dot(const vec2d &v) const {
+  return ((x * v[0]) + (y * v[1]));
+}
+
+vec2d vec2d::cap(double scalar_val) const {
+  //"cap" a vector's magnitude at a certain value
+
+  if(this->magnitude() > scalar_val) {
+    //too big
+    return (this->normalize() * scalar_val);
+  }
+  else {
+    //not too big
+    return *this;
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+//and then there's this one just hangin' around down here
+
+std::ostream &operator<<(std::ostream &output, const vec2d &v) {
+  output << "(" << v[0] << ", " << v[1] << ")";
+  return output;
+}
--- vec2d/vec2d.h
+++ vec2d/vec2d.h
@@ -1,25 +1,63 @@
 #ifndef VEC2D_H_
 #define VEC2D_H_
 
+#include <iostream>
 
 class vec2d {
   //a 2d vector class, for position and velocity
 public:
   vec2d() : x(0), y(0) { }
   vec2d(double xin, double yin) : x(xin), y(yin) { }
-  vec2d(vec2d& v) : x(v[0]), y(v[1]) { }
+  vec2d(const vec2d& v) : x(v[0]), y(v[1]) { }
+  vec2d& operator=(const vec2d &v) {
+    if(this != &v) {
+      x = v[0];
+      y = v[1];
+    }
+    return *this;
+  }
 
 
   //these operators let us return values with array notation
   double &operator [](int i);
   double operator [](int i) const;
 
+  //these return a vector modified in a scalar way
+  vec2d operator+(double scalar_val) const;
+  vec2d operator-(double scalar_val) const;
+  vec2d operator*(double scalar_val) const;
+  vec2d operator/(double scalar_val) const;
+  vec2d operator-() const; //negation
+
+  //these return a vector modified by another vector
+  vec2d operator+(const vec2d &v) const;
+  vec2d operator-(const vec2d &v) const;
+  vec2d operator*(const vec2d &v) const;
+  //not implementing divide, no way to do that safely since
+  //x or y could be 0
+
+  //these modify the vector in question itself in a scalar way
+  //TODO: +=, -=, *=? may not even need these
+
+  double magnitude() const;
+  double magnitudeSquared() const;
+  vec2d normalize() const; //NOTE: will not normalize beyond .001, returns this
+
+  double dot(const vec2d &v) const;
+  //vec2d cross(const vec2d &v) const; //it's a 2d game, this isn't implemented
+
+  vec2d cap(double scalar_val) const;
+  
+
+
+
+
 private:
   double x;
   double y;
 };
 
-
+std::ostream &operator<<(std::ostream &output, const vec2d &v);
 
 
 #endif
