0ccd625f27d02a5cf2368d46ae099d2d184142bb
Nick Reed <donovanreed418@gmail.com>
2019-07-27 19:47:03 -0400

    moved unused files to zscraps

--- zscraps/texture.cpp
+++ zscraps/texture.cpp
@@ -1,11 +1,13 @@
 #include "texture.h"
 #include "render.h"
+#include "src/utils/message.h"
+#include <string>
 
 texture::texture(std::string path) : g_texture(NULL) {
 
   SDL_Surface *s = IMG_Load(path.c_str());
   if( s == NULL ) {
-    std::cerr << "Couldn't create image \"" << path << "\"! Error: " << SDL_GetError() << std::endl;
+    msg::print_error("couldn't create image \"" + path + "\"! Error: " + std::string(SDL_GetError()));
   }
   else {
     //make the texture from the surface's pixels
