22188911b99c5ca00417c5f54f45ef25c7a7109b
Nick Reed <donovanreed418@gmail.com>
2019-07-27 19:47:37 -0400

    made a new class for pretty-printing in the terminal... and promptly abandoned it.

---
+++ zscraps/print_color_const.cpp
@@ -0,0 +1,12 @@
+#include "print_color_const.h"
+#include <string>
+
+const std::string pcc::rn = "\033[0;31m";
+const std::string pcc::rb = "\033[1;31m";
+const std::string pcc::yn = "\033[0;33m";
+const std::string pcc::yb = "\033[1;33m";
+const std::string pcc::gn = "\033[0;32m";
+const std::string pcc::gb = "\033[1;32m";
+const std::string pcc::cn = "\033[0;36m";
+const std::string pcc::cb = "\033[1;36m";
+const std::string pcc::none = "\033[0m";
---
+++ zscraps/print_color_const.h
@@ -0,0 +1,21 @@
+#ifndef PRINT_COLOR_CONST_
+#define PRINT_COLOR_CONST_
+
+#include <string>
+
+class pcc {
+public:
+  static const std::string rn;
+  static const std::string rb;
+  static const std::string yn;
+  static const std::string yb;
+  static const std::string gn;
+  static const std::string gb;
+  static const std::string cn;
+  static const std::string cb;
+  static const std::string none;
+
+  
+};
+
+#endif
