f8f2cbf26a53a409c37eee0611a931f340f573b7
Nick Reed <donovanreed418@gmail.com>
2019-08-19 02:38:00 -0400

    added midground and foreground

--- src/environment/background/background.cpp
+++ src/environment/background/background.cpp
@@ -7,4 +7,6 @@ b_ground::~b_ground() { }
 
 void b_ground::draw() const {
   image_handler::get().draw_tile(bg_name + "bg", 0.1);
+  image_handler::get().draw_tile(bg_name + "mg", 0.4);
+  image_handler::get().draw_tile(bg_name + "fg", 1.2);
 }
