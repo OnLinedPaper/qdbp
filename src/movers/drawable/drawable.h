c16e678215205dc3bc2842851fbf8085ada28d8a
Nick Reed <donovanreed418@gmail.com>
2019-09-13 00:35:17 -0400

    moved update logic to cpp file

--- src/movers/drawable/drawable.h
+++ src/movers/drawable/drawable.h
@@ -3,19 +3,27 @@
 
 #include "src/movers/movable.h"
 #include "src/image/image.h"
+#include "src/xml_parser/xmlparse.h"
 #include <SDL2/SDL.h>
 
 class drawable : public movable {
 public:
-  drawable(const std::string im) : image_name(im), frame_bump(rand()) { }
+  drawable(const std::string path) :
+    movable(path),
+    image_name("/" + xmlparse::get().get_xml_string(path + "/textures/body")),
+    frame_bump(rand()),
+    last_angle(0)
+  { }
 
 protected:
-  void update() override { movable::update(); }
+  void update() override;
   std::string image_name;
 
   //this is so when the image is drawn, each one has a different
   //frame
   double frame_bump;
+
+  double last_angle;
 
 
 };
