735d7e843f8a16734cda28c286b13da31ca9d596
Nick Reed <donovanreed418@gmail.com>
2019-09-13 01:16:46 -0400

    added collision (overlap) logic, and split the draw function up into multiple versions for drawing coordinates or specific hitboxes

--- src/rect2d/rect2d.cpp
+++ src/rect2d/rect2d.cpp
@@ -4,9 +4,23 @@
 #include "src/viewport/viewport.h"
 
 void rect2d::draw() const {
+  coord_draw(
+    (tlc[0] - viewport::get().get_tlc_x()),
+    (tlc[1] - viewport::get().get_tlc_y())
+  );
+}
+
+void rect2d::tlc_draw() const {
+  coord_draw(
+    tlc[0] - viewport::get().get_tlc_x(),
+    tlc[1] - viewport::get().get_tlc_y()
+  );
+}
+
+void rect2d::coord_draw(double x, double y) const {
   SDL_Rect r;
-  r.x = (tlc[0] - viewport::get().get_tlc_x()) + dims[0] / 2;
-  r.y = (tlc[1] - viewport::get().get_tlc_y()) + dims[1] / 2;
+  r.x = x;
+  r.y = y;
   r.w = dims[0];
   r.h = dims[1];
 
@@ -20,14 +34,12 @@ void rect2d::draw() const {
   SDL_RenderFillRect(render::get().get_r(), &r);
   SDL_SetRenderDrawColor(render::get().get_r(), 64, 255, 64, 255);
   SDL_RenderDrawRect(render::get().get_r(), &r);
-
-
 }
 
 void rect2d::draw(int red, int green, int blue) const {
   SDL_Rect r;
-  r.x = (tlc[0] - viewport::get().get_tlc_x()) + dims[0] / 2;
-  r.y = (tlc[1] - viewport::get().get_tlc_y()) + dims[1] / 2;
+  r.x = (tlc[0] - viewport::get().get_tlc_x());
+  r.y = (tlc[1] - viewport::get().get_tlc_y());
   r.w = dims[0];
   r.h = dims[1];
 
@@ -41,4 +53,58 @@ void rect2d::draw(int red, int green, int blue) const {
   SDL_RenderFillRect(render::get().get_r(), &r);
   SDL_SetRenderDrawColor(render::get().get_r(), red, green, blue, 255);
   SDL_RenderDrawRect(render::get().get_r(), &r);
+}
+
+bool rect2d::overlap(const rect2d &r) const {
+
+  /*
+   *consider the rectangles below. they project "shadows" onto the
+   *x-axis (below) and y-axis (beside). If both of the shadows
+   *overlap, the rectangles overlap. If one of the shadows doesn't
+   *overlap, the rectangles don't overlap.
+   *
+   *if either end of the # rectangle's shadow falls between the % shadow,
+   * they collide. use this to calculate whether rectangles collide or not.
+   *
+   * tlc[1] > %        %---------%          tlc > %---------%
+   *          |        |         |                |         |
+   *          | #      |    #---------#           |         |   #---------#
+   * brc[1] > % |      %----|----%    |           %---------%   |         |
+   *            |           |         |    vs           brc ^   |         |
+   *            |           |         |                         |         |
+   *            #           #---------#                         #---------#
+   *
+   *                        #---------#                         #---------#
+   *                   %---------%                %---------%
+   *                                       tlc[0] ^  brc[0] ^
+  */
+
+
+
+  //this rect
+  vec2d ttlc = this->get_tlc();
+  vec2d tbrc = this->get_brc();
+
+  //other rect
+  vec2d rtlc = r.get_tlc();
+  vec2d rbrc = r.get_brc();
+
+
+  //check if one of the rectangle's corners falls in the "shadow" of the other
+  if(
+    //leftmost x of other rect between least and greatest x of this rect
+    (rtlc[0] >= ttlc[0] && rtlc[0] <= tbrc[0]) ||
+    //rightmost x of other rect between least and greatest x of this rect
+    (rbrc[0] >= ttlc[0] && rbrc[0] <= tbrc[0])
+  ) {
+    //the x-shadows overlap, now see if the y-shadows overlap
+    if(
+      (rtlc[1] >= ttlc[1] && rtlc[1] <= tbrc[1]) ||
+      (rbrc[1] >= ttlc[1] && rbrc[1] <= tbrc[1])
+    ) {
+      //both shadows overlap
+      return true;
+    }
+  }
+  return false;
 }
--- src/rect2d/rect2d.h
+++ src/rect2d/rect2d.h
@@ -15,13 +15,21 @@ public:
 
   void set_tlc(const vec2d &t) { tlc = t; }
   void set_center(const vec2d &c) {
-    tlc[0] = c[0] - dims[0];
-    tlc[1] = c[1] - dims[1];
+    tlc[0] = c[0] - (dims[0] / 2);
+    tlc[1] = c[1] - (dims[1] / 2);
   }
+  vec2d get_dims() const { return dims; }
   void set_dims(const vec2d &d) { dims = d; }
 
   void draw() const;
   void draw(int, int, int) const;
+  void tlc_draw() const;
+  void coord_draw(double, double) const;
+
+  bool overlap(const rect2d &r) const;
+
+  vec2d get_tlc() const { return tlc; }
+  vec2d get_brc() const { return vec2d(tlc[0] + dims[0], tlc[1] + dims[1]); }
 
 private:
   vec2d tlc;
