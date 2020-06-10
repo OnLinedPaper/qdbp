0b565062a156cf7b8b4660aa2177af4ccb025ddf
Nick Reed <donovanreed418@gmail.com>
2020-06-09 23:55:45 -0400

    laid the groundwork for weapons, which will behave much like entities in that they can be easily recycled behind teh scenes to save on memory

---
+++ src/movers/drawable/hittable/weapon.cpp
@@ -0,0 +1,75 @@
+#include "weapon.h"
+#include "src/timeframe/timeframe.h"
+#include "src/image/image_handler.h"
+
+weapon::weapon(const std::string path) :
+  hittable(path),
+  heat_per_shot(xmlparse::get().get_xml_float(
+    path + "/shot/heat_per_shot"
+  )),
+  ms_lifespan(xmlparse::get().get_xml_float(
+    path + "/shot/ms_lifespan"
+  )),
+  dist_lifespan(xmlparse::get().get_xml_float(
+    path + "/shot/dist_lifespan"
+  )),
+  inaccuracy(xmlparse::get().get_xml_float(
+    path + "/shot/inaccuracy"
+  )),
+  life_ms_mod(1),
+  life_dist_mod(1),
+  con_vel(xmlparse::get().get_xml_float(
+    path + "/shot/constant_vel"
+  ))
+{ this->set_active(false); }
+
+weapon::~weapon()
+{ }
+
+void weapon::draw() const {
+  image_handler::get().draw_rotate(image_name, pos[0], pos[1], 0, last_angle);
+}
+
+//check to see if the weapon can fire (time based) and then
+//return the heat required to shoot it
+float weapon::fire(const vec2d start_pos, const vec2d start_vel, 
+    const vec2d angle,
+    float heat_mod, float life_ms_mod,
+    float life_dist_mod, float inacc_mod,
+    float vel_mod) {
+
+  //spawn projectile 
+  set_pos(start_pos);
+
+  //now, create the additional speed it will use
+  vec2d add_vel = angle.normalize() * (con_vel * vel_mod);
+
+  set_vel(start_vel + add_vel);
+
+  //save these for when updating the projectile
+  this->life_ms_mod = ms_lifespan * life_ms_mod;
+  this->life_dist_mod = dist_lifespan * life_dist_mod;
+
+
+  //bring this projectile into update and draw cycles
+  set_active(true);
+
+  return heat_per_shot * heat_mod;
+}
+
+void weapon::update() {
+  //check to see if the projectile has outlived its distance or time
+  if(life_ms_mod <= 0 || life_dist_mod <= 0) {
+    set_active(false);
+  }
+  else {
+    hittable::update();
+
+    //subtract time
+    life_ms_mod -= t_frame::get().get_elapsed();
+
+    //subtract distance
+    life_dist_mod -= vel.magnitude();
+  }
+
+}
---
+++ src/movers/drawable/hittable/weapon.h
@@ -0,0 +1,68 @@
+#ifndef WEAPON_H_
+#define WEAPON_H_
+
+#include "src/movers/drawable/hittable/hittable.h"
+
+/*
+this is intended to act as an independent projectile - upon firing a weapon,
+one of these is generated and begins its movement.
+the projectile lifetime and other things are to be decided
+elsewhere and passed in, though these will have default values; current
+theory is a default value is loaded and a multiplier is passed in. 
+THESE WILL BE RECYCLED. it'd be a hassle to delete and reallocate these things
+over and over, so upon "destruction" the projectile will just become invisible
+and upon "creation" will be made visible and given new coordinates/locations.
+*/
+
+class weapon : public hittable {
+public:
+  weapon(const std::string path);
+  ~weapon();
+
+  void draw() const override;
+  virtual void update() override;
+
+  //returns the generated heat from this shot
+  float fire(const vec2d start_pos, const vec2d start_vel, 
+      const vec2d angle, 
+      float heat_mod, float life_ms_mod, 
+      float life_dist_mod, float inacc_mod,
+      float vel_mod);
+
+  void move_up() override {}
+  void move_dn() override {}
+  void move_lf() override {}
+  void move_rt() override {}
+
+
+private:
+  //the heat generated per shot
+  float heat_per_shot;
+  //the delay between shots in ms
+  float ms_lifespan;
+  //the distance the projectile can travel before expiring
+  float dist_lifespan;
+  //the random variance the projectile has on spawn
+  float inaccuracy;
+
+
+  //below are "modifiers" the projectile will use for this current "life"
+
+  //the velocity modifier is NOT saved since it's only used for firing 
+  //float vel_mod;
+  //heat modifier is NOT saved since it's only used on firing
+  //float heat_mod;
+  //the time it has to live
+  float life_ms_mod;
+  //the distance it has to live
+  float life_dist_mod;
+  //inaccuracy modifier is NOT saved since it's only used on firing
+  //float inacc_mod;
+
+  //the constant velocity of this projectile
+  float con_vel;
+
+  std::string xml_path = "/movers/hittable/";
+};
+
+#endif
