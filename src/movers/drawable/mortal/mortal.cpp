2067e3d4b7e165d7ecab9621f215f1991525edb9
paper <donovanreed418@gmail.com>
2021-05-17 01:37:36 -0400

    new class added that combines hittable, heatable, and killable - still wip

---
+++ src/movers/drawable/mortal/mortal.cpp
@@ -0,0 +1,147 @@
+#include "src/movers/drawable/mortal/mortal.h"
+#include "src/rect2d/rect2d.h"
+#include "src/entity_handler/entity_handler.h"
+#include "src/utils/message.h"
+
+//==== CONSTRUCTORS ===========================================================
+
+mortal::mortal(const std::string path) :
+  drawable(path, vec2d(0,0), vec2d(0,0)),
+
+  tangible(true),
+
+  //get max health - default to 1 np, which creates the entity
+  max_health(xmlparse::get().safe_get_xml_float(
+    path + "/health/health_max", 1
+  )),
+  curr_health(max_health),
+
+  //get health segments - default to 1, which makes a single bar
+  health_segments(xmlparse::get().safe_get_xml_float(
+    path + "/health/health_segments", 1
+  )),
+
+  is_regenerating(false),
+  //get regen rate - default to 0, meaning no regen
+  h_regen_rate(xmlparse::get().safe_get_xml_float(
+    path + "/health/health_regen_rate", 0
+  )),
+
+  //get regen heat gen - default to 0, making regen free with no heat cost
+  h_regen_heat_gen(xmlparse::get().safe_get_xml_float(
+    path + "/health/health_regen_heat", 0
+  )),
+
+  //get max shield segments - default to 0, meaning no shields
+  max_shield_segments(xmlparse::get().safe_get_xml_int(
+    path + "/health/shield_segments", 0
+  )),
+  curr_shield_segments(max_shield_segments),
+
+  //get regen rate - default to 0, meaning no shield regen
+  s_regen_rate(xmlparse::get().safe_get_xml_float(
+    path + "/health/shield_regen_rate"
+  )),
+
+  //get first shield size - arbitrarily make first size 0.5?
+  first_s_size(xmlparse::get().safe_get_xml_float(
+    path + "/health/shield_first_seg_size"
+  )) 
+{ }
+
+mortal::mortal(const std::string path, const vec2d &p, const vec2d &v) :
+  drawable(path, p, v)
+{
+  //load all hitboxes into their vectors
+  for(std::string p : xmlparse::get().get_all_child_tags(path + "/hitboxes")) {
+    hy_box h(path + "/hitboxes/" + p);
+
+    if(h.get_type() == hitbox::TYPE_HITBOX) {
+      hitboxes.push_back(h);
+    }
+    else if(h.get_type() == hitbox::TYPE_HURTBOX) {
+      hurtboxes.push_back(h);
+    }
+    else if(h.get_type() == hitbox::TYPE_WEAKBOX) {
+      weakboxes.push_back(h);
+    }
+    else if(h.get_type() == hitbox::TYPE_ARMORBOX) {
+      armorboxes.push_back(h);
+    }
+    else if(h.get_type() == hitbox::TYPE_SHIELDBOX) {
+      shieldboxes.push_back(h);
+    }
+    else if(h.get_type() == hitbox::TYPE_PICKUPBOX) {
+      pickupboxes.push_back(h);
+    }
+    else if(h.get_type() == hitbox::TYPE_VACUUMBOX) {
+      vacuumboxes.push_back(h);
+    }
+  }
+
+  //all the vectors are ready - push it into one big vector
+  //(they're ordered this way so they draw nicely when debugging)
+  boxes.push_back(&vacuumboxes);
+  boxes.push_back(&hurtboxes);
+
+  boxes.push_back(&weakboxes);
+  boxes.push_back(&hitboxes);
+  boxes.push_back(&armorboxes);
+  boxes.push_back(&shieldboxes);
+
+  boxes.push_back(&pickupboxes);
+
+  //the boxes want to be updated and calibrated before they load properly
+  update_boxes();
+  calibrate_boxes();
+}
+
+//==== GENERAL TRAITS =========================================================
+
+void mortal::update() {
+  drawable::update();
+
+  //update the hitboxes
+  mortal::update_boxes();
+}
+
+void mortal::draw() const {
+  drawable::draw();
+  if(e_handler::get().get_draw_debug_info()) {
+    draw_boxes();
+  }
+}
+
+void mortal::draw_boxes() const {
+  for(std::vector<hy_box> &h : *v) {
+    h.draw();
+  }
+}
+
+//==== HITBOX AND COLLISION ===================================================
+
+void mortal::calibrate_boxes() {
+  //after creating / recycling a hittable, the boxes are recalibrated to 
+  //prevent them from drawing lines to their last position or "snapping" there
+  //unexpectedly
+  for(std::vector<hy_box> *v : boxes) {
+    for(hy_box &h : *v) {
+      h.calibrate();
+    }
+  }
+}
+
+void mortal::update_boxes() {
+  //after every other update is done, update the boxes last so they don't lag
+  //behind the model of the ship
+
+  //update their locations based on their positions - the boxes have their own
+  //update logic related to how fast they're moving
+  for(std::vector<hy_box> *v : boxes) {
+    for(hy_box &h : *v) {
+      h.set_box_center(pos, last_angle);
+    }
+  }
+}
+
+//==== DAMAGE AND HEALTH ======================================================
---
+++ src/movers/drawable/mortal/mortal.h
@@ -0,0 +1,153 @@
+#ifndef MORTAL_H_
+#define MORTAL_H_
+
+//the mortal class is a refactored combination of the hittable and killable
+//classes. it contains hybrid boxes that can register hits, and tracks data 
+//for health and shielding, as well. this is likely going to encompass every
+//entity the player is designed to interact with. 
+//TODO: it may also become the parent class of weapons for ease of use, but 
+//this is yet to be determined
+
+#include "src/movers/drawable/drawable.h"
+#include "src/rect2d/hitbox/hitbox.h"
+#include "src/rect2d/hitbox/hitline.h"
+#include "src/rect2d/hitbox/hybrid_box.h"
+#include "src/xml_parser/xmlparse.h"
+#include <vector>
+
+class mortal : public drawable {
+public:
+//==== GENERAL MOVABLE TRAITS =================================================
+
+  mortal(const std::string &path);
+  mortal(const std::string &path, const vec2d &pos, const vec2d &vel);
+  virtual ~mortal() = default;
+
+  //overridden here because the hitboxes must be updated each time the ship
+  //jumps to a new location
+  void set_pos(const vec2d &p) { this->set_pos(p[0], p[1]); }
+  void set_pos(float x, float y);
+
+  void draw() const override;
+  void draw_boxes() const;
+
+  virtual void update() override;
+
+//==== HITBOX AND COLLISION ===================================================
+
+  //takes an incoming box, line, hybrid, or even mortal, and checks for a 
+  //collision with a specific type of hitbox
+  virtual bool collides(const hitbox &h, int type) const;
+  virtual bool collides(const hitline &l, type) const;
+  virtual bool collides(const hy_box &hy, int type) const;
+  virtual bool collides(const mortal *other, int other_type, int this_type) const;
+
+  bool is_tangible() const { return active && tangible; }
+
+  void calibrate_boxes();
+
+//==== DAMAGE AND HEALTH ======================================================
+  
+  float get_health() const { return curr_health; }
+  float get_health_percent() const { return curr_health / max_health; }
+  float get_shields() const { return curr_shield_segments; }
+  float get_shield_percent() const { return curr_shields / max_shields; }
+
+  bool is_regen() const { return is_regenerating; }
+  void toggle_regen() { is_regenerating = !is_regenerating; }
+
+  //damage scales depending on which box was struck
+  virtual bool take_damage(float damage, int box_type_hit);
+
+  //when health hits 0, the entity usually becomes intangible and begins a
+  //death animation of some sort before becoming invisible. this handles
+  //such an event.
+  virtual void perish();
+
+//=============================================================================
+//=============================================================================
+
+protected:
+//==== HITBOX AND COLLISION ===================================================
+
+  //returns a reference to a subset of total boxes
+  std::vector<hy_box> const &get_box_ref(int type) const;
+  
+  void update_boxes();
+
+  //holds the vectors of the other boxes for ease of updating
+  std::vector<std::vector<hy_box> *> boxes;
+
+  //boxes are split into 1 + 6 "types" 
+  //- hitboxes deal damage and are commonly found on weapons
+  //
+  //- hurtboxes take normal damage and are commonly found on entities, and are
+  //    usually ABSENT from weapons unless they're destroyable en route
+  //- weakboxes take increased damage
+  //- armorboxes take reduced damage
+  //- shieldboxes take no damage (but still deplete shields if hit)
+  //- pickupboxes are used for item entities that can be collected
+  //- vacuumboxes are used to collide with pickupboxes to absorb them
+  std::vector<hy_box> hitboxes;
+
+  std::vector<hy_box> hurtboxes;
+  std::vector<hy_box> weakboxes;
+  std::vector<hy_box> armorboxes;
+  std::vector<hy_box> shieldboxes;
+  std::vector<hy_box> pickupboxes;
+  std::vector<hy_box> vacuumboxes;
+
+  //"tangibility" is an entity's ability to be struck. all inactive entities
+  //are intangible - common other reasons for intangibility are death
+  //animations, where the entity no longer blocks shots, but is still being 
+  //rendered and updated and is thus not yet "inactive". 
+  //intangible entities do not check for collision and cannot interact with
+  //other hitboxes.
+  bool tangible;
+
+//==== DAMAGE AND HEALTH ======================================================
+
+  //health operates on a "bars" principle.
+  //if health is divided into, for example, 5 bars, the player can only 
+  //regenerate up to the current bar, and if they fall below a bar's threshold,
+  //they cannot passively regenerate up to it again.
+  //however, players can only lose up to two bars at a time - massive damage
+  //can remove the current bar and the next one, but no more than that. more 
+  //bars means less passive regen but also less damage from massive attacks.
+  //passive healing is toggled, and generates heat.
+  float max_health;
+  float curr_health;
+  int health_segments;
+  bool is_regenerating;
+  float h_regen_rate;
+  float h_regen_heat_gen;
+
+  //shields operate on a similar "bars" principle, with a slight tweak.
+  //ANY damage destroys 1 and only 1 bar of shield, be it small or massive.
+  //shields constantly regenerate without generating heat. the last bar of
+  //shield is much larger than the others, meaning if all shields are lost, 
+  //they take much longer to recharge, but as long as at least 1 bar remains,
+  //the remainder will return quickly.
+  float max_shields = 100;  //always locked at 100 since shields are bar based
+  float curr_shields = 100;  
+  int max_shield_segments;  //total number of segments, can be 0
+  int curr_shield_segments;  //current number of segments, can be 0
+  float s_regen_rate;  //how quick shields restore themselves
+  float first_s_size;  //a number from 0 to 1 representing how much of the
+                       //shield bar is taken up by the first segment 
+
+  //damage being dealt can be scaled depending on what box is hit. as a general
+  //rule, weakboxes take extra damage and armorboxes take reduced damage. 
+  //the damage taken by boxes is adjustable both on
+  //initial spawn and later on during gameplay. in practice this means the 
+  //player and some enemies can adjust how resistant they are to damage, and
+  //more powerful enemies can scale their damage resistance based on the player
+  float weakbox_scale = 4;  //usually 4x damage in a weak box
+  float hurtbox_scale = 1;  //usually 1x damage in a hurt box
+  float armorbox_scale = .25;  //usually 1/4x damage in an armor box
+
+private: 
+
+};
+
+#endif
