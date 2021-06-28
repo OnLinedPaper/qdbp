c5c0096c3e4b1413d269254c566b5ce788006a2b
paper <donovanreed418@gmail.com>
2021-06-28 10:46:53 -0400

    fixed missing include header

--- src/movers/drawable/mortal/debug_entities/debug_killable.h
+++ src/movers/drawable/mortal/debug_entities/debug_killable.h
@@ -1,9 +1,11 @@
 #ifndef MORTAL_DEBUG_KILLABLE_H_
 #define MORTAL_DEBUG_KILLABLE_H_
 
+#include "src/movers/drawable/mortal/mortal.h"
+
 class d_killable : public mortal {
 public:
-  d_killable(const std::string);
+  d_killable(const std::string&);
 
   void update() override;
   void draw() const override;
