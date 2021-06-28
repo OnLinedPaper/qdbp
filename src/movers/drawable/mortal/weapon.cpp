5a94d2e5cf1889df1c10cf42cacd3205ddbf16af
paper <donovanreed418@gmail.com>
2021-06-28 10:44:46 -0400

    added weapon, a subclass of mortal, which is designed to more completely encapsulate weapon-like traits

---
+++ src/movers/drawable/mortal/weapon.cpp
@@ -0,0 +1,183 @@
+#include <random>
+
+#include "src/movers/drawable/mortal/weapon.h"
+#include "src/timeframe/timeframe.h"
+#include "src/image/image_handler.h"
+#include "src/utils/message.h"
+
+  std::map<uint8_t, float> weapon::id_to_heat;
+  std::map<uint8_t, float> weapon::id_to_delay;
+
+weapon::weapon(const std::string path) :
+  mortal(path),
+  type_id(xmlparse::get().get_xml_int(
+    path + "/shot/unique_type_id"
+  )),
+  life_tick(xmlparse::get().get_xml_float(
+    path + "/shot/tick_lifespan"
+  )),
+  life_dist(xmlparse::get().get_xml_float(
+    path + "/shot/dist_lifespan"
+  )),
+  inaccuracy(xmlparse::get().get_xml_float(
+    path + "/shot/inaccuracy"
+  )),
+  life_tick_mod(1),
+  life_dist_mod(1),
+  con_vel(xmlparse::get().get_xml_float(
+    path + "/shot/constant_vel"
+  )),
+  impact_damage(xmlparse::get().get_xml_float(
+    path + "/damage/impact_damage"
+  )),
+  impact_mod(1),
+  armor_piercing(xmlparse::get().get_xml_int(
+    path + "/damage/armor_penetration"
+  )),
+  armor_pierce_mod(armor_piercing)
+
+weapon::~weapon()
+{ }
+
+float weapon::get_heat_from_id(uint8_t id) {
+  if(id_to_heat.find(id) == id_to_heat.end()) {
+    //id wasn't found - make entry to prevent errors
+    id_to_heat.insert({id, 0});
+
+    //send message to prevent silent failure
+    msg::print_warn(
+        ("weapon with id " + std::to_string(id) + " has no heat value assigned to it")
+    );
+  }
+
+  return(id_to_heat[id]);
+}
+
+float weapon::get_delay_from_id(uint8_t id) {
+  if(id_to_delay.find(id) == id_to_delay.end()) {
+    //id wasn't found - make entry to prevent errors
+    id_to_delay.insert({id, 1});
+
+    //send message to prevent silent failure
+    msg::print_warn(
+        ("weapon with id " + std::to_string(id) + " has no delay value assigned to it")
+    );
+  }
+}
+
+void weapon::strike_target(mortal &target, int box_type) {
+  //depends on weapon - the vast majority will just do damage, but more exotic
+  //effects like healing or slowdowns may apply
+
+  target->take_damage(damage * damage_mod, box_type);
+  armor_pierce_mod -= 1;
+  if(armor_pierce_mod < 0) {
+    this->perish();
+  }
+
+  return;
+}
+
+void weapon::preload_weapon_data() {
+  std::string path = "/movers/weapons";
+
+  //get all weapon names
+  std::vector<std::string> weap_names = xmlparse::get().get_all_child_tags(path);
+
+  //cycle through all collected names and load their data
+  //(assume all names are unique and warn the user if they aren't)
+  for(std::string s : weap_names) {
+    float id = xmlparse::get().get_xml_float(
+      path + "/" + s + "/shot/unique_type_id"
+    );
+
+    //sanity check - does this id exist yet in the arrays?
+    if(id_to_heat.find(id) != id_to_heat.end() 
+        || id_to_delay.find(id) != id_to_delay.end()) {
+      //this id is already in the array, warn the user and skip this iteration
+      msg::print_warn("weapon with id " + std::to_string(id) + " and name " 
+          + s + " already exists in memory! (is the name unique?)");
+      continue;
+    }
+
+    float heat = xmlparse::get().get_xml_float(
+      path + "/" + s + "/shot/heat_per_shot"
+    );
+    float delay = xmlparse::get().get_xml_float(
+      path + "/" + s + "/shot/tick_shot_delay"
+    );
+
+    //load the values
+    id_to_heat.insert({id, heat});
+    id_to_delay.insert({id, delay});
+  }
+
+  return;
+}
+
+void weapon::draw() const {
+  image_handler::get().draw_rotate_color(
+      image_name, pos[0], pos[1], 0, last_angle, team_col
+  );
+}
+
+void weapon::fire(
+  const vec2d &start_pos, const vec2d &start_vel, const vec2d &angle,
+  float heat_mod,
+  float life_tick_mod, float life_dist_mod,
+  float inacc_mod,
+  float vel_mod,
+  int pierce_mod, float damage_mod,
+  const SDL_Color &c
+) {
+  //spawn projectile
+  set_pos(start_pos);
+
+  std::random_device rd;
+  std::uniform_int_distribution<> distrib(-31, 31);
+
+  //get a random vec
+  vec2d randm(distrib(rd), distrib(rd));
+
+  //create velocity, using velocity of parent and weapon, plus some randomness
+  vec2d add_vel = (
+    (angle.normalize() * (1 - inaccuracy)) +
+    (randm.normalize() * (inaccuracy))
+  ).normalize() * (con_vel * vel_mod);
+
+  set_vel(start_vel + add_vel);
+
+  //save these for updating projectile
+  this->life_tick_mod = tick_lifespan * life_tick_mod;
+  this->life_dist_mod = dist_lifespan * life_dist_mod;
+
+  //save the color
+  set_col(c);
+
+  //reset armor piercing
+  armor_pierce_mod = armor_piercing + pierce_mod;
+
+  //bring projectile into collision, update, and draw cycles
+  set_tangible(true);
+  set_active(true);
+
+  return; 
+}
+
+void weapon::update() {
+  //check to see if projectile went too far or lived too long
+  if(life_tick_mod <= 0 || life_dist_mod <= 0) {
+    this->perish();
+  }
+  else {
+    mortal::update();
+
+    //subtract time, compensating for lag - slower game ticks means faster 
+    //projectile movement and quicker drain on life ticks
+    life_tick_mod -= 
+        t_frame::get().get_elapsed_t() * t_frame::get().t_adjust();
+
+    //subtract distance, which has already had any lag factored in
+    life_dist_mod -= vel.magnitude();
+  }
+}
---
+++ src/movers/drawable/mortal/weapon.h
@@ -0,0 +1,84 @@
+#ifndef MORTAL_WEAPON_H_
+#define MORTAL_WEAPON_H_
+
+#include "src/movers/drawable/mortal/mortal.h"
+
+/*
+this is an independent projectile, and is handled differently than normal
+mortals, with different data as well. most notably, these are recycled and
+reused in memory instead of deleted and reallocated; on destruction, it is
+removed from update and draw queues, and upon "creation" is given new data
+*/
+
+class weapon : public mortal {
+public:
+  weapon(const std::string path);
+  ~weapon();
+
+  //these are universally accessible tables of basic weapon data
+  static float get_heat_from_id(uint8_t);
+  static float get_delay_from_id(uint8_t);
+  static void preload_weapon_data();
+
+  void draw() const override;
+  virtual void update() override;
+
+  void fire(const vec2d start_pos, const vec2d start_vel,
+    const vec2d angle,
+    float heat_mod, float life_ms_mod,
+    float life_dist_mod, float inacc_mod,
+    float vel_mod, int pierce_mod,
+    float damage_mod, const SDL_Color &);
+
+  bool is_type(uint8_t type_id) const {
+    return !(type_id ^ this->type_id);
+  }
+
+  virtual void strike_target(mortal &, int);
+  float get_damage() { return impact_damage * impact_damage_mod; }
+
+  void set_pos(const vec2d &p) { this->set_pos(p[0], p[1]); }
+  void set_pos(float x, float y) { mortal::set_pos(x, y); }
+
+private:
+
+  //private maps let other classes access heat-per-shot and
+  //delay-per-shot data
+  static std::map<uint8_t, float> id_to_heat;
+  static std::map<uint8_t, float> id_to_delay;
+
+  //the unique id of this weapon
+  uint8_t type_id;
+
+  //the number of ticks the shot can last before expiring
+  float life_tick;
+  //the distance the projectile can go before expiring
+  float life_dist;
+  //the random variance from point of aim at spawn
+  float inaccuracy;
+
+  //below are constant data pieces
+  //projectile's base velocity
+  float con_vel;
+  //how much damage is dealt on hit
+  float impact_damage;
+  //how many box types it can hit
+  int armor_pierce;
+
+  //below are "modifiers" the projectile uses for its current "life"
+  //time to live
+  float life_tick_mod;
+  //distance to live
+  float dist_tick_mod;
+  //damage modifier
+  float impact_damage_mod;
+  //extra piercing
+  int armor_pierce_mod;
+
+  //TODO: change to "/movers/mortal/weapon" or something?
+  std::string xml_path = "/movers/hittable/";
+};
+
+
+
+#endif
