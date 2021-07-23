349b96312aa2ba6047f7219c83188a92d6045389
paper <donovanreed418@gmail.com>
2021-07-23 16:24:48 -0400

    finally adding heat and the like to player

--- src/movers/drawable/mortal/player.h
+++ src/movers/drawable/mortal/player.h
@@ -18,15 +18,74 @@ public:
   virtual void move_lf() = 0;
   virtual void move_rt() = 0;
 
+  void boost(bool b);
+
+  bool is_overheated() { return is_overheat; }
+
   void draw() const override;
   virtual void update() override;
 
-  //TODO: implement later
-  bool is_overheat() { return false; }
 
 protected:
-  
-//heat, shield heat gen, health heat gen, overheat, weapon, 
+
+//==== HEAT ===================================================================  
+
+  //the player possesses a unique system, "heat" - this is basically a resource
+  //pool that depletes as the player perorms actions, such as shooting or 
+  //regenerating. heat naturally reduces itself over time, and can be forcibly
+  //removed through a few means, such as powerups
+
+  //the following actions increase heat:
+  // - moving at "boosted" speed (high)
+  // - firing a weapon (med-high)
+  // - regenerating health (low)
+  // - activating a portal (low)
+
+  //the following actions do not generate heat:
+  // - moving at normal speed
+  // - regenerating shields 
+
+  //the following actions reduce heat:
+  // - "venting" heat (ship cannot perform actions besides moving)
+
+  //players also have a unique mechanic, "overheat". the max heat is not
+  //actually a hard limiter; players can overheat their ships as well, pushing
+  //their systems past redline. this causes some consequences in gameplay,
+  //typically involving sensitive systems that can't work when overheated, and
+  //if the player exceeds their overheat as well, their ship takes damage and
+  //"burns out", forcibly activates venting until ALL heat is gone
+
+//---- basics -----------------------------------------------------------------
+
+  float heat;                       //the total amount of heat in the ship
+  float max_heat;                   //max normal heat, starts overheating after
+  float max_overheat;               //burnout occurs if it reaches this point
+
+  float boost_heat_per_tick;        //heat gen for boosting
+  float regen_heat_per_tick;        //heat gen for regenerating
+
+  float cool_per_tick;              //heat lost naturally over time
+  float vent_cool_per_tick;         //heat lost through venting
+  float vent_startup_tick_delay;    //time delay before venting starts
+
+//---- modifiers --------------------------------------------------------------
+
+  float max_heat_mod;               //can heat more before overheat
+  float max_overheat_mod;           //can heat more before burnout
+
+  float boost_heat_per_tick_mod;    //higher numbers, more heat when boosting
+  float regen_heat_per_tick_mod;    //higher numbers, more heat when regen
+
+  float cool_per_tick_mod;          //higher numbers, more cooling per tick
+  float vent_cool_per_tick_mod;     //higher numbers, more cooling per tick
+  float vent_startup_tick_delay_mod;  //higher numbers, more time before vent
+
+//---- booleans ---------------------------------------------------------------
+
+  bool is_overheat;                 //past max_heat, hasn't reached burnout
+  bool is_burnout;                  //exceeded max_overheat and is burnt out
+  bool is_vent;                     //is venting heat
+  bool is_boost;                    //is boosting, which raises velocity cap
 
 private:
 
