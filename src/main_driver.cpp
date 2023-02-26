7fe8971f6bd6c784eec6cfb157bee341e4dec31b
paper <donovanreed418@gmail.com>
2023-02-26 16:18:47 -0500

    confirmed stack overflow is coming from xml parser

--- src/main_driver.cpp
+++ src/main_driver.cpp
@@ -1,6 +1,20 @@
 #include "main_driver.h"
 
+//-
+//TODO: remove once the stack overflow is gone
+#include "src/xml_parser/xmlparse.h"
+#include <iostream>
+//-
+
 int main(void) {
+//-
+  //TODO: remove once the stack overflow is gone
+  std::cout << "building" << std::endl;
+  xmlparse::get().build_tree("resources/gamedata.xml");
+  std::cout << "built" << std::endl;
+  return 0;
+//-
+
   engine main_engine;
   main_engine.play();
 
