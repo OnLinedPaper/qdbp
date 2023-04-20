b736a5d1e56dd24d63535b1069eb6ffa269d99f6
paper <donovanreed418@gmail.com>
2023-04-20 03:45:32 -0400

    added orientation to lines

--- src/rect2d/hitbox/hitline_nc.cpp
+++ src/rect2d/hitbox/hitline_nc.cpp
@@ -22,11 +22,33 @@ char hitline::angle_to_char() const {
 switch vars around to ensure run is always positive
 calculate slope, and then: 
 
--2 to +2, and inf: |
-+2 to +0.5: /
-+0.5 to -0.5: -
--0.5 to -2: \
+-2.5 to +2.5, and inf:  |
++2.5 to +0.4:           /
++0.4 to -0.4:           -
+-0.4 to -2.5:           \
+
+112 to 68:  |
+68 to 22:   /
+23 to 338:  -
+338 to 292: \
+
 */
+
+  //lp is left point; rp is right point
+  //also multi by 10 to meet slope requirements
+  bool p1_left = start[0] < end[0];
+  int lp_x = (p1_left ? start[0] : end[0]);
+  int lp_y = (p1_left ? start[1] : end[1]);
+  int rp_x = (p1_left ? end[0] : start[0]);
+  int rp_y = (p1_left ? end[1] : start[1]);
+
+  int rise = rp_y - lp_y;
+  int run = rp_x - lp_x;  //never negative
+
+  if(run == 0 || -25 * run > 10 * rise || 10 * rise >= 25 * run) { return '|'; }
+  if(25 * run > 10 * rise && 10 * rise >= 4 * run) { return '\\'; }
+  if(4 * run > 10 * rise && 10 * rise >= -4 * run) { return '-'; }
+  if(-4 * run > 10 * rise && 10 * rise >= -25 * run) { return '/'; }
 
   return '=';
 }
